3fec911e111faccdd5e538248423389974b65848
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-03-17 12:15:03 -0600

    style: Add finished style to LogIn modal

--- src/js/views/loginView.js
+++ src/js/views/loginView.js
@@ -6,25 +6,33 @@ class LoginView {
     this._overlay = document.querySelector('.overlay');
     this._window = document.querySelector('.login--window');
     this._loginInputs = document.querySelectorAll('.login--input');
-    // this._parentElement = document.querySelector('.lgmodal');
+    this._parentElement = document.querySelector('.login--container');
     this._addHandlerLogin();
   }
   toggleOverlay() {
     this._overlay.classList.toggle('hidden');
     this._window.classList.toggle('hidden');
   }
-  toggleFocus() {
-    console.log(e.target);
+  toggleFocus(e) {
+    const input = e.target;
+    const div = input.closest('div');
+    if (input.value.trim() === '') {
+      input.value = '';
+      div.classList.contains('focused')
+        ? div.classList.remove('focused')
+        : div.classList.add('focused');
+    }
   }
   _addHandlerLogin() {
     this._btnOpen.addEventListener('click', this.toggleOverlay.bind(this));
     this._btnClose.addEventListener('click', this.toggleOverlay.bind(this));
     this._overlay.addEventListener('click', this.toggleOverlay.bind(this));
-    this._loginInputs.forEach((input) =>
-      input.addEventListener('focus', function (e) {
-        // this.toggleFocus();
-      })
-    );
+    this._loginInputs.forEach((input) => {
+      input.addEventListener('focusin', this.toggleFocus.bind(this));
+    });
+    this._loginInputs.forEach((input) => {
+      input.addEventListener('focusout', this.toggleFocus.bind(this));
+    });
   }
 }
 
