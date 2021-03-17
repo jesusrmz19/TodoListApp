01c58536e96b5b9813a2038410941d558fafb37b
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-03-17 12:18:37 -0600

    Fix local storage bug that was not updating the storage when you deleted a todo

--- src/js/model.js
+++ src/js/model.js
@@ -1,50 +1,51 @@
 import { async } from 'regenerator-runtime';
 
-export const state  = {
-    todo: {
-        value: '',
-        id: '',
-        checked: false
-    },
-    todolist: []
+export const state = {
+  todo: {
+    value: '',
+    id: '',
+    checked: false,
+  },
+  todolist: [],
 };
 
 const persistTodolist = function () {
-    localStorage.setItem('todolist', JSON.stringify(state.todolist));
+  localStorage.setItem('todolist', JSON.stringify(state.todolist));
 };
 
-const createId = function(){
-    return Date.now() + ''.slice(-10);
-}
-
-export const addTodo = function(todo) {
-    state.todo = {
-        value: todo,
-        id: createId(),
-        checked: false
-    };
-    state.todolist.push(state.todo);
-    persistTodolist();
+const createId = function () {
+  return Date.now() + ''.slice(-10);
 };
 
-export const deleteTodo = function(id) {
-    const index = state.todolist.findIndex(el => el.id === id);
-    state.todolist.splice(index, 1);
-}
+export const addTodo = function (todo) {
+  state.todo = {
+    value: todo,
+    id: createId(),
+    checked: false,
+  };
+  state.todolist.push(state.todo);
+  persistTodolist();
+};
 
-export const updateTodo = function(checked, id) {
-    const index = state.todolist.findIndex(el => el.id === id.slice(5));
-    state.todolist[index].checked = checked;
-    persistTodolist();
-}
+export const deleteTodo = function (id) {
+  const index = state.todolist.findIndex((el) => el.id === id);
+  state.todolist.splice(index, 1);
+  persistTodolist();
+};
+
+export const updateTodo = function (checked, id) {
+  const index = state.todolist.findIndex((el) => el.id === id.slice(5));
+  state.todolist[index].checked = checked;
+  persistTodolist();
+};
 
 const init = function () {
-    const todolist = localStorage.getItem('todolist');
-    if (todolist) state.todolist = JSON.parse(todolist);
+  const todolist = localStorage.getItem('todolist');
+  if (todolist) state.todolist = JSON.parse(todolist);
 };
 init();
 
 const clearList = function () {
-    localStorage.clear('todolist');
+  localStorage.clear('todolist');
 };
 // clearList();
